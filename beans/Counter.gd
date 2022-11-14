a79acb3e6db677d51eec911a8c444b4acad11194
Frank <fmerriman27@gmail.com>
2022-11-14 13:43:57 +1100

    first demo of bean catching

---
+++ beans/Counter.gd
@@ -0,0 +1,34 @@
+extends RichTextLabel
+
+
+var total_time = 0
+var stopwatch_running = true
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#Start timer at 0 seconds
+	self.text = String(total_time)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	if stopwatch_running == true:
+		#Add time since last frame to total_time
+		total_time += delta
+		
+		#Update scoreboard (with rounding)
+		var rounded_time = stepify(total_time, 0.001)
+		self.text = String(rounded_time)
+
+
+#Toggle stopwatch
+func _on_Button_pressed():
+	if stopwatch_running == true:
+		stopwatch_running = false
+		print("stopped timer")
+	elif stopwatch_running == false:
+		stopwatch_running = true
+		print("started timer")
+		
---
+++ beans/HumanHand.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+export var speed = 400
+var screen_size
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("movement_right"):
+		velocity.x += 1
+	elif Input.is_action_pressed("movement_left"):
+		velocity.x -=1
+		
+	#Normalise vectors (overkill probably since only left/right possible)
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed	
+	
+	#Update position
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
