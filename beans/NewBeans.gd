1aa2cffee36cdc5f54f5d50d0959326b3b57ccb4
FrankMerriman <fmerriman27@gmail.com>
2022-11-29 15:27:48 +1100

    first version of sequential random cans being made complete

---
+++ beans/NewBeans.gd
@@ -0,0 +1,68 @@
+extends Timer
+
+var characters = ["Bluford", "Ewart", "Ferdo", "Gorm"]
+var next_beans = {
+	"source" : "", 
+	"target" : ""
+	}
+
+# All signals run code in head.gd, but each signal only triggers
+# an action from one of the four head nodes in the gamescene
+signal bluford_trigger()
+signal ewart_trigger()
+signal ferdo_trigger()
+signal gorm_trigger()
+
+func _ready():
+	# Randomise the seed of the random number generator
+	randomize()
+	# Create initial source and target
+	generate_new_source_and_target()
+
+# Will generate information determining from which character the next bean
+# will spawn, and which character will be the target the beans are meant to
+# be delivered to
+func _on_NewBeans_timeout():
+	
+	# Emit signal for current source and target pair
+	if next_beans["source"] == "Bluford":
+		emit_signal("bluford_trigger", next_beans["target"])
+	elif next_beans["source"] == "Ewart":
+		emit_signal("ewart_trigger", next_beans["target"])
+	elif next_beans["source"] == "Ferdo":
+		emit_signal("ferdo_trigger", next_beans["target"])
+	elif next_beans["source"] == "Gorm":
+		emit_signal("gorm_trigger", next_beans["target"])
+	
+	# Update the signal for next firing
+	generate_new_source_and_target()
+
+# Update the next source/target pair by generating two indicies
+# to select characters from our character list
+func generate_new_source_and_target():
+	var source_index = randi()%4
+	var target_index = randi()%4
+	
+	# Continue to regenerate target_index until it is not the same
+	# as source index
+	while target_index == source_index:
+		target_index = randi()%4
+	
+	# Now we have two different characters and we can update the next_beans
+	# dictionary
+	set_source(characters[source_index])
+	set_target(characters[target_index])
+	
+	
+
+func get_source():
+	return next_beans["source"]
+	
+func set_source(source):
+	next_beans["source"] = source
+
+func get_target():
+	return next_beans["target"]
+	
+func set_target(target):
+	next_beans["target"] = target
--- beans/Scripts/Head.gd
+++ beans/Scripts/Head.gd
@@ -6,8 +6,7 @@ var can = preload("res://Scenes/BeanCan.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var speech = $"SpeechBubble"
-	speech.hide()
+	$"SpeechBubble".hide()
 
 
 func launch_can():
@@ -38,19 +37,51 @@ func facing_right():
 		
 	return face_right
 	
-func flash_speech_bubble(source_head):
-	var speech = $"SpeechBubble"
-	var hide_timer = $"SpeechBubble/HideBubble"
-	var target = $"SpeechBubble/TargetHead"
+
+# Change the sprite show in the speech bubble to match the target
+# of the can about to be created
+func update_speech_bubble(target):
+	var head_sprite = $"SpeechBubble/TargetHead"
 	
-	if source_head == "Bluford":
-		target.set_bluford()
-		speech.show()
-		hide_timer.start()
+	if target == "Bluford":
+		head_sprite.set_bluford()
+	elif target == "Ewart":
+		head_sprite.set_ewart()
+	elif target == "Ferdo":
+		head_sprite.set_ferdo()
+	elif target == "Gorm":
+		head_sprite.set_gorm()
+	
+
+# Displays the speech bubble to the player before auto-hiding
+func flash_speech_bubble():
+	#Access the speech bubble sprite and run its show method
+	$"SpeechBubble".show()
+	#Acess the hide bubble timer and run its start method
+	$"HideBubble".start()
 	
 	
 
-# Launch a can towards center of game
-func _on_NewBeans_timeout(source_head):
-	flash_speech_bubble(source_head)
+func create_new_can(target_head):
+	update_speech_bubble(target_head)
+	flash_speech_bubble()
 	launch_can()
+
+
+# These signals were declared in the body of NewBeans.gd
+# I then connected them through the editor (non-advanced mode)
+# I then manually changed the below functions to accept 1 argument
+# When I emit the signals, I send a single argument (target_head) and
+# it all works out fine. Hopefully this summary helps get your head around custom
+# signals, because the documentation is pretty light on all of this.
+func _on_NewBeans_bluford_trigger(target_head):
+	create_new_can(target_head)
+
+func _on_NewBeans_ewart_trigger(target_head):
+	create_new_can(target_head)
+
+func _on_NewBeans_ferdo_trigger(target_head):
+	create_new_can(target_head)
+
+func _on_NewBeans_gorm_trigger(target_head):
+	create_new_can(target_head)
